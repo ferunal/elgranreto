f0489812d2ca67914cb2818442283c66ab956933
lchacon <lchacon@alquilado-59996.SIMPLE.local>
2015-10-20 16:41:36 -0500

    Todas las operaciones, validar diferentes  escenarios de prueba

--- src/co/edu/poli/ingesoft2/granreto/FachadaGranReto.java
+++ src/co/edu/poli/ingesoft2/granreto/FachadaGranReto.java
@@ -6,10 +6,13 @@
 package co.edu.poli.ingesoft2.granreto;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -24,11 +27,14 @@ public class FachadaGranReto implements IFachadaGranReto {
 
     private List<String> lstFilasArchivo;
     Path pathRutaArchivoValida;
+    private boolean expresionValida;
 
     public FachadaGranReto() {
         lstFilasArchivo = new ArrayList<>();
+        expresionValida = false;
     }
 
+    //<editor-fold defaultstate="collapsed" desc="Validaciones">
     public boolean esNumero(String string) {
         if (string == null || string.isEmpty()) {
             return false;
@@ -86,6 +92,13 @@ public class FachadaGranReto implements IFachadaGranReto {
         return true;
     }
 
+    public boolean esOperacion(String str) {
+        if (str.length() != 1) {
+            return false;
+        }
+        return str.contains("+") || str.contains("-") || str.contains("*") || str.contains("/") || str.contains("^");
+    }
+
     public boolean esNotacionCientifica(String string) {
         if (string == null || string.isEmpty()) {
             return false;
@@ -102,6 +115,75 @@ public class FachadaGranReto implements IFachadaGranReto {
         if ((cantidadOcurrenciasCaracter(string, ',') == 1 && cantidadOcurrenciasCaracter(string, 'E') == 1)
                 && (cantidadOcurrenciasCaracter(string, '+') == 1) || cantidadOcurrenciasCaracter(string, '-') == 1) {
             return esNumero(string.replace("-", "").replace("+", "").replace("E", "").replace(",", ""));
+        } else {
+            return false;
+        }
+    }
+//</editor-fold>
+
+    public BigDecimal operacion(BigDecimal opr1, BigDecimal opr2, String operacion) {
+        BigDecimal resultado;
+        switch (operacion) {
+            case "+":
+                resultado = opr1.add(opr2);
+                break;
+            case "-":
+                resultado = opr1.subtract(opr2);
+                break;
+            case "*":
+                resultado = opr1.multiply(opr2);
+                break;
+            case "/":
+                if (opr2.equals(BigDecimal.ZERO)) {
+                    return null;
+                } else {
+                    resultado = opr1.divide(opr2);
+                }
+                break;
+            case "^":
+                resultado = opr1.pow(opr2.intValue());
+                break;
+            default:
+                return null;
+
+        }
+        return resultado;
+    }
+
+    private boolean validarNumerosExpresion(Integer posicion) {
+
+        if (esNotacionCientifica(lstFilasArchivo.get(posicion))) {
+            lstFilasArchivo.set(posicion, DecimalFormat.getInstance().format(Double.parseDouble(lstFilasArchivo.get(posicion).replace(",", "."))));
+            return true;
+        } else if (esNumeroConComas(lstFilasArchivo.get(posicion))) {
+            lstFilasArchivo.set(posicion, DecimalFormat.getInstance().format(Double.parseDouble(lstFilasArchivo.get(posicion).replace(",", "."))));
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private boolean validarPosicionesOperador(Integer posicion) {
+
+        return esOperacion(lstFilasArchivo.get(posicion));
+
+    }
+
+    private boolean validarContenido() {
+        boolean numeros = true;
+        boolean operadores = true;
+        if (expresionValida) {
+            numeros = validarNumerosExpresion(0);
+            numeros = validarNumerosExpresion(1);
+            numeros = validarNumerosExpresion(3);
+            numeros = validarNumerosExpresion(4);
+            numeros = validarNumerosExpresion(7);
+            operadores = validarPosicionesOperador(2);
+            operadores = validarPosicionesOperador(5);
+            operadores = validarPosicionesOperador(6);
+            operadores = validarPosicionesOperador(8);
+            return numeros && operadores;
         } else {
             return false;
         }
@@ -124,11 +206,14 @@ public class FachadaGranReto implements IFachadaGranReto {
                 }
             }
 
-            for (String filaArchivoLeida : lstFilasArchivo) {
-                System.out.println(filaArchivoLeida);
-            }
+            expresionValida = lstFilasArchivo.size() == 9;
+//
+//            for (String filaArchivoLeida : lstFilasArchivo) {
+//                System.out.println(filaArchivoLeida);
+//            }
 
         } catch (IOException ex) {
+            expresionValida = false;
             Logger.getLogger(FachadaGranReto.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -141,6 +226,7 @@ public class FachadaGranReto implements IFachadaGranReto {
             cargarFilasArchivo();
 
         } else {
+            expresionValida = false;
             throw new GranRetoException("Error de entrada-salida", new Throwable("El archivo no existe, no es legible, es una carpeta o no es un archivo regular."));
         }
 
@@ -148,8 +234,32 @@ public class FachadaGranReto implements IFachadaGranReto {
 
     @Override
     public String calcular() {
-        return "";
+        if (validarContenido()) {
+            String numero1 = lstFilasArchivo.get(0);
+            String numero2 = lstFilasArchivo.get(1);
+            String operacion1 = lstFilasArchivo.get(2);
+            String numero3 = lstFilasArchivo.get(3);
+            String numero4 = lstFilasArchivo.get(4);
+            String operacion2 = lstFilasArchivo.get(5);
+            String operacion3 = lstFilasArchivo.get(6);
+            String numero5 = lstFilasArchivo.get(7);
+            String operacion4 = lstFilasArchivo.get(8);
 
+            BigDecimal resultado1 = operacion(new BigDecimal(numero1), new BigDecimal(numero2), operacion1);
+            BigDecimal resultado2 = operacion(new BigDecimal(numero3), new BigDecimal(numero4), operacion2);
+            BigDecimal resultado3 = operacion(resultado1, resultado2, operacion3);
+            BigDecimal resultado4 = operacion(resultado3, new BigDecimal(numero5), operacion4);
+            String strResultado = NumberFormat.getIntegerInstance().format(resultado4.doubleValue());
+            StringBuilder strBResultado = new StringBuilder(strResultado);
+            if (strResultado.length() > 8) {
+
+                strBResultado.replace(strBResultado.length() - 8, strBResultado.length() - 7, "'");
+
+            }
+            return strBResultado.toString();
+        } else {
+            return "ERROR";
+        }
     }
 
 }
--- src/elgranretojuancarlosrico/ElGranRetoJuanCarlosRico.java
+++ src/elgranretojuancarlosrico/ElGranRetoJuanCarlosRico.java
@@ -8,6 +8,9 @@ package elgranretojuancarlosrico;
 import co.edu.poli.ingesoft2.granreto.FachadaGranReto;
 import co.edu.poli.ingesoft2.granreto.GranRetoException;
 import java.io.File;
+import java.math.BigDecimal;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,10 +28,16 @@ public class ElGranRetoJuanCarlosRico {
         
         try {
             FachadaGranReto fachadaGranReto = new FachadaGranReto();
-            System.out.println(fachadaGranReto.esNumeroConComas("253,2"));
-            
-            String ruta = System.getProperty("user.home") + File.separator + "archivo1.txt";
+//            System.out.println(fachadaGranReto.esNotacionCientifica("253,2-E25"));
+//            System.out.println(fachadaGranReto.operacion(new BigDecimal("15.2"), new BigDecimal(2), "-"));
+//          //  System.out.println( String.format("%.d", 132546465));
+//            DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+//            dfs.setDecimalSeparator(',');
+//            dfs.setPerMill('.');
+           
+            String ruta = System.getProperty("user.home") + File.separator + "prueba.txt";
             fachadaGranReto.cargarArchivo( ruta);
+            System.out.println(fachadaGranReto.calcular());
         } catch (GranRetoException ex) {
             Logger.getLogger(ElGranRetoJuanCarlosRico.class.getName()).log(Level.SEVERE, null, ex);
         }
